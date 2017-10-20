40d61e01cf6402ce9f9be0e951f77b1315f08e0c
Eric Wooley <ericwooley@gmail.com>
2017-10-19 19:55:18 -0700

    Got email working

--- contribute-response.js
+++ contribute-response.js
@@ -12,22 +12,25 @@ module.exports = function(context, cb) {
   try {
     const payload = JSON.parse(context.body.payload)
     const repoName = payload.repository.full_name
+    console.log('api key stuff', context.secrets)
     const mailer = mailgun(({apiKey: context.secrets.MAILGUN_API_KEY, domain: context.secrets.MAILGUN_DOMAIN}))
     const userApi = payload.sender.url + '?access_token=' + context.secrets.GITHUB_ACCESS_TOKEN
-    console.log('checkign api', userApi)
     return Promise.all([
       getContent(repoName),
       getUserEmail(userApi)
     ])
     .then(values => {
-      console.log('got values', values)
-      return ({email: values[1], text: values[0], html: values[0].html})
+      return ({email: values[1], text: values[0].text, html: values[0].html})
     })
-    .then((values) => sendEmail(mailer.messages().send, values.email, values.text, values.html))
-    .then(() => )
-    .catch(e => cb(null, e.message || "unknown error"))
+    .then((values) => console.log('made it to here', values) || values)
+    .then((values) => sendEmail(mailer, values.email, values.text, values.html))
+    .then((body) => cb(null, body))
+    .catch(e =>{
+      console.log('catch error', e, e.stack)
+      cb(null, e.message || "unknown error")
+    })
   } catch (e) {
-    console.log('error', e)
+    console.log('startup error', e)
     cb(null, e.message || 'unknown error');  
   }
 };
@@ -38,20 +41,16 @@ const getUserEmail = (userUrl) =>
 const getContent = (repoName) => 
   fetch(`https://raw.githubusercontent.com/${repoName}/master/CONTRIBUTING.md`)
     .then(response => response.text())
-    .then((text) => ({text, html: marked(text)}))
+    .then((text) => ({text: text, html: marked(text)}))
     
-const sendEmail = (mailSender, to, text, html) => 
-  new Promise((resolve, reject) => 
-    mailSender({
+const sendEmail = (mailSender, to, text, html) => {
+    console.log('sending mail', config.from, to, config.subject, text, html)
+    mailSender.messages().send({ 
       from: config.from,
-      to,
+      to: to,
       subject: config.subject,
-      text,
-      html
-    }, function (error, body) {
-      if(error) return reject(error)
-      resolve(body)
-    }
-  )
-)
+      // text: text, // breaks when you do both? Maybe becuase this version of mailgun-js is very old?
+      html: html
+    })
+}
   
